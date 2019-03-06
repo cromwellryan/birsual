2bd43d5326caee102765ea8fcf5643bdfaab95eb
Ryan Cromwell <ryan@cromwellhaus.com>
2019-03-06 09:45:53 -0500

    feat (About): Some content in that thar about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,15 @@ import Layout from '../components/Layout'
 
 const About = () => (
   <Layout>
-    <p>About 1 Question</p>
+    <h1>1 Question</h1>
+    <p>1 Question arose out of collecting interesting questions and
+      conversation starters for one-on-one meetings that Ryan Cromwell has with
+      team members. Questions </p>
+
+    <h2>Credit to:</h2>
+    <ul className="credits">
+      <li className="credit"><a href="https://jasonevanish.com/2014/05/29/101-questions-to-sk-in-1-on-1s/">Jason Evanish</a> for so many wonderful questions, without with Ryan's one-on-one's would not have been nearly as valuable.</li>
+    </ul>
   </Layout>
 )
 
