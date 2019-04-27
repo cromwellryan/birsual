495b078e2e0030d9aa4c97cd736f207671237cdb
Ryan Cromwell <ryan@cromwellhaus.com>
2019-04-26 22:24:07 -0400

    feat (About): Define domain and share origin story

--- pages/about.js
+++ pages/about.js
@@ -2,16 +2,13 @@ import Layout from '../components/Layout'
 
 const About = () => (
   <Layout>
-    <div className="container">
-      <h1>1 Question</h1>
-      <p>1 Question arose out of collecting interesting questions and
-        conversation starters for one-on-one meetings that Ryan Cromwell has with
-        team members. Questions </p>
+    <div className="about container">
+      <h1>About Birsual.com</h1>
+      <p>"Bir Sual" means "question" in Azerbaijani. Birsual.com arose from 
+        collecting interesting questions and conversation starters for 
+        one-on-one meetings that Ryan Cromwell has with team members.</p>
 
-      <h2>Credit to:</h2>
-      <ul className="credits">
-        <li className="credit"><a href="https://jasonevanish.com/2014/05/29/101-questions-to-sk-in-1-on-1s/">Jason Evanish</a> for so many wonderful questions, without which Ryan's one-on-one's would not have been nearly as valuable.</li>
-      </ul>
+      <p>Credit to <a href="https://jasonevanish.com/2014/05/29/101-questions-to-sk-in-1-on-1s/">Jason Evanish</a> for so many wonderful questions, without which Ryan's one-on-one's would not have been nearly as valuable.</p>
     </div>
   </Layout>
 )
