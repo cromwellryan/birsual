522c3cb02898fa3fc0b965c3cb9942d47a9b78cf
Ryan Cromwell <ryan@cromwellhaus.com>
2019-06-19 11:42:50 -0400

    feat: favicons for days

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,15 @@ const Layout = (props) => (
       <link href="/static/styles.css" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" />
 
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+      <link rel="manifest" href="/static/site.webmanifest"/>
+      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#a55bd5"/>
+      <link rel="shortcut icon" href="/static/favicon.ico"/>
+      <meta name="msapplication-TileColor" content="#ffffff"/>
+      <meta name="msapplication-config" content="/static/browserconfig.xml"/>
+      <meta name="theme-color" content="#ffffff"/>
     </Head>,
     <Header />,
     <main className="main">
