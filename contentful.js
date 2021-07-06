b4e6a2c91e60bb12fb2fc43c79003ab7f88bc5eb
Ryan Cromwell <ryan@heysparkbox.com>
2021-07-05 23:37:53 -0400

    fix (Contentful): Secrets in environment variables

--- contentful.js
+++ contentful.js
@@ -1,3 +1,3 @@
-export const spaceId = 'b4vil0hzqfzo';
-export const accessToken = '741af704ed2f8d2e9d7df3ca230134d4972923c154213026e0e01f91af0813ac';
-export const environmentId = 'master';
+export const spaceId = process.env.SPACE_ID;
+export const accessToken = process.env.ACCESS_TOKEN;
+export const environmentId = process.env.ENVIRONMENT_ID;
